1c2fc10818cf4a72cce79b0bd7ae6cd5b0c2005d
Nakamura Yukiko <okirakugama@yahoo.co.jp>
2021-06-15 18:05:33 +0900

    jquery 追加

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.burger-btn').on('click',function(){
+    $('.burger-btn').toggleClass('close');
+    $('.nav-wrapper').toggleClass('slide-in');
+    $('body').toggleClass('noscroll');
+  });
+  
+});
